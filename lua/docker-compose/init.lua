704d7ec58f694d7b4570945aa7f35755b7b9d459
Sébastien NOBILI <code@pipoprods.org>
2024-03-28 16:35:32 +0100

    chore: format file

--- lua/docker-compose/init.lua
+++ lua/docker-compose/init.lua
@@ -3,83 +3,83 @@ local M = {}
 local Terminal = require("toggleterm.terminal").Terminal
 
 local STATES = {
-  UP = "up",
-  DOWN = "down",
+	UP = "up",
+	DOWN = "down",
 }
 local state = STATES.DOWN
 
 -- Check if file exists
 function M.file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
 end
 
 -- Start services
 function M.up()
-  vim.notify("Starting containers...")
+	vim.notify("Starting containers...")
 
-  local t = Terminal:new({ cmd = "docker-compose up", hidden = false })
-  t:spawn()
+	local t = Terminal:new({ cmd = "docker-compose up", hidden = false })
+	t:spawn()
 
-  state = "up"
+	state = "up"
 end
 
 -- Stop services
 function M.down()
-  if state == STATES.UP then
-    print("Stopping containers...")
-    vim.cmd([[silent !docker-compose down]])
-    vim.cmd([[echon '']])
-    state = STATES.DOWN
-  end
+	if state == STATES.UP then
+		print("Stopping containers...")
+		vim.cmd([[silent !docker-compose down]])
+		vim.cmd([[echon '']])
+		state = STATES.DOWN
+	end
 end
 
 -- Kill services
 function M.kill()
-  if state == STATES.UP then
-    print("Killing containers...")
-    vim.cmd([[silent !docker-compose kill]])
-    vim.cmd([[echon '']])
-    state = STATES.DOWN
-  end
+	if state == STATES.UP then
+		print("Killing containers...")
+		vim.cmd([[silent !docker-compose kill]])
+		vim.cmd([[echon '']])
+		state = STATES.DOWN
+	end
 end
 
 -- Setup plugin
 function M.setup()
-  vim.api.nvim_create_autocmd("DirChangedPre", {
-    callback = function()
-      if M.file_exists("docker-compose.yaml") or M.file_exists("docker-compose.yml") then
-        M.kill()
-      end
-    end,
-  })
-  vim.api.nvim_create_autocmd("DirChanged", {
-    callback = function()
-      if M.file_exists("docker-compose.yaml") or M.file_exists("docker-compose.yml") then
-        M.up()
-      end
-    end,
-  })
+	vim.api.nvim_create_autocmd("DirChangedPre", {
+		callback = function()
+			if M.file_exists("docker-compose.yaml") or M.file_exists("docker-compose.yml") then
+				M.kill()
+			end
+		end,
+	})
+	vim.api.nvim_create_autocmd("DirChanged", {
+		callback = function()
+			if M.file_exists("docker-compose.yaml") or M.file_exists("docker-compose.yml") then
+				M.up()
+			end
+		end,
+	})
 
-  vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-      if M.file_exists("docker-compose.yaml") or M.file_exists("docker-compose.yml") then
-        M.up()
-      end
-    end,
-  })
-  vim.api.nvim_create_autocmd("VimLeavePre", {
-    callback = function()
-      if M.file_exists("docker-compose.yaml") or M.file_exists("docker-compose.yml") then
-        M.kill()
-      end
-    end,
-  })
+	vim.api.nvim_create_autocmd("VimEnter", {
+		callback = function()
+			if M.file_exists("docker-compose.yaml") or M.file_exists("docker-compose.yml") then
+				M.up()
+			end
+		end,
+	})
+	vim.api.nvim_create_autocmd("VimLeavePre", {
+		callback = function()
+			if M.file_exists("docker-compose.yaml") or M.file_exists("docker-compose.yml") then
+				M.kill()
+			end
+		end,
+	})
 end
 
 return M
